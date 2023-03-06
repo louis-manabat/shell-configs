4918bc1b5d5766f37f59840bde1429b69102a252
louis-manabat <manabat.online@gmail.com>
2023-03-06 19:54:05 +1100

    Added new functions to check versioning of P10K and ZSH

---
+++ setup-functions/checkP10KVersion.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/zsh
+#
+# ZSH Version script was created on = ZSH 5.8
+#
+# Modular script to check package/plugins if they exist or at a reasonable version level
+
+retval=0
+
+if [ ! -d "$1" ]; then
+	printf "Powerlevel10K is not installed. Please install Powerlevel10k below:\\
+			Repository link: https://github.com/romkatv/powerlevel10k" >/dev/null 2>&1
+	retval=1
+else
+	printf 'Powerlevel10k is installed' >/dev/null 2>&1
+	retval=0
+fi
+
+echo "$retval"
