4874ded9f1db9a6ca5b8b5ce96de767d854dd4b6
louis-manabat <manabat.online@gmail.com>
2023-03-06 22:56:23 +1100

    Work in progress for installation and stuff of packages

--- installation_2.sh
+++ installation_2.sh
@@ -3,16 +3,19 @@
 # ZSH Version script was created on = ZSH 5.8
 
 echo '>--------------------------'
-echo '> Installing Python3 & Pip3'
-echo '>--------------------------'
-sudo apt-get install libffi-dev libssl-dev
-sudo apt install python3-dev
-sudo apt-get install -y python3 python3-pip
-
-echo '>--------------------------'
 echo '> Installing Docker-Compose'
 echo '>--------------------------'
 sudo pip3 install docker-compose
 
 echo 'Enabling Docker on boot'
 sudo systemctl enable docker
+
+if [ "$rpi" = true ]; then
+    echo 'Installing relevant Raspberry Pi packages'
+fi
+
+echo '>--------------------------------'
+echo '> Installing pokemon-colorscripts'
+echo '>--------------------------------'
+git submodule update --remote --merge
+sudo ./submodules/pokemon-colorscripts/install.sh
