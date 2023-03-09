3ec997b73c5086506d432464042db13ccb517cb5
louis-manabat <manabat.online@gmail.com>
2023-03-09 21:50:05 +1100

    Zsh: Added aliases file

---
+++ dotfiles/zsh/aliases.zsh
@@ -0,0 +1,4 @@
+(($+commands[git])) && { alias g="git" }
+(($+commands[docker])) && { alias d="docker" }
+(($+commands[docker-compose])) && { alias dc="docker-compose" }
+(($+commands[kubectl])) && { alias k3s="kubectl" }
