5ca64db7470bb3286fdaceea789b0fd65faa20ea
Louis Manabat <42296961+louis-manabat@users.noreply.github.com>
2023-08-18 09:41:53 +1000

    Update aliases.zsh
    Added py3 as an alias python3

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -3,3 +3,4 @@
 (($+commands[docker-compose])) && { alias dc="docker-compose" }
 (($+commands[kubectl])) && { alias k3s="kubectl" }
 (($+commands[terraform])) && { alias tf="terraform" }
+(($+commands[python3])) && { alias py3="python3" }
