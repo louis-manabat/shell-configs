2cb0dc4be678f63b285b79f59a364bf7c4276a53
louis-manabat <manabat.online@gmail.com>
2023-12-14 14:56:50 +1100

    Working on it

---
+++ shell-scripts/git-submodules-init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+#
+# ZSH Version script was created on = ZSH 5.8
+
+echo $1
+
+# # Initialising the Git submodules + quick sleep
+# git submodule init
+# sleep 5
+
+# # Updating the Git submodules
+# git submodule update
